dbd53cf64589289e9c0b2630a79eddc6235bad05
yaswanthkumarch <yaswanthkumarch2001@gmail.com>
2025-09-18 08:46:09 +0530

    updated naem

--- backend/routes/Notes.js
+++ backend/routes/Notes.js
@@ -6,6 +6,8 @@ const {
   createNote, getNotes, updateNote, deleteNote
 } = require('../controllers/notesController');
 
+
+
 router.post('/', auth, createNote);
 router.get('/', auth, getNotes);
 router.put('/:id', auth, updateNote);
